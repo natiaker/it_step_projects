6fa3334b543b8bc3e30a5bf6e3b149394517c4d4
natiaker <n.kerdzaia@gmail.com>
2024-03-04 16:38:16 +0400

    Add comments

--- ATM/account.py
+++ ATM/account.py
@@ -3,28 +3,38 @@ import json
 ACCOUNT_FILE = "accounts.json"
 
 
+# define Account class
 class Account:
+    # Initialize an Account object with account number, name, balance and pin
     def __init__(self, account_number, name, balance, pin):
         self.account_number = account_number
         self.name = name
         self.balance = balance
         self.pin = pin
 
+    # return a string representation of Account object
     def __str__(self):
         return (f"Account number: {self.account_number}, Name: {self.name}, Balance: {self.balance}, "
                 f"PIN: {self.pin}")
 
+    # get the current balance of the account
     def getbalance(self):
         return self.balance
 
+    # get the account number
     def get_account_number(self):
         return self.account_number
 
+    # deposit specified amount of money into the balance
+    # check deposit amount to be greater than 0
     def deposit(self, amount):
         if amount <= 0:
-            raise ValueError("Amount must be greater than 0")
-        self.balance += amount
+            print("Amount must be greater than 0")
+        else:
+            self.balance += amount
 
+    # withdraw specified amount of money from balance
+    # check the amount to be greater than 0 and check if there is enough money on balance to withdraw
     def withdraw(self, amount):
         if amount <= 0:
             print("Amount must be greater than 0")
@@ -33,44 +43,56 @@ class Account:
         else:
             self.balance -= amount
 
+    # change pin with new pin
     def change_pin(self, pin):
         self.pin = pin
 
+    # return current pin
     def get_pin(self):
         return self.pin
 
 
+# custom serialization method to translate Account class into json format
 def serialization_func(obj):
+    # check if the object is an Account class type
     if isinstance(obj, Account):
         return {"account_number": obj.account_number, "name": obj.name, "balance": obj.balance, "pin": obj.pin}
     raise TypeError(f'Not type of Account')
 
 
+# custom deserialization method to translate json file into Account class instance
 def deserialization_func(obj):
     return Account(obj["account_number"], obj["name"], obj["balance"], obj["pin"])
 
 
+# define account manager class
 class AccountManager:
+    # initialize empty account list
     def __init__(self):
         self.account_list = []
 
+    # write accounts in json file using custom deserialization method
     def write_accounts(self):
         with open(ACCOUNT_FILE, 'w') as json_file:
             json.dump(self.account_list, json_file, default=serialization_func, indent=4)
 
+    # read accounts from json file and save it in account_list
     def read_accounts(self):
         with open(ACCOUNT_FILE, 'r') as json_file:
             new_account_list = json.load(json_file, object_hook=deserialization_func)
         self.account_list = new_account_list
 
+    # method to append user input account to the account list
     def add_account(self, account):
         self.account_list.append(account)
 
+    # check if the user input account exists in the account list
     def check_account(self, account_number):
         for account in self.account_list:
             if account.get_account_number() == account_number:
                 return account
         return None
 
+    # method to remove specified account from the account list
     def delete_account(self, account):
         self.account_list.remove(account)
--- ATM/main.py
+++ ATM/main.py
@@ -3,9 +3,11 @@ import random
 from ATM.account import Account, AccountManager
 
 
+# function for the homepage menu
+# the user has the option to either log in or create a new account - using numbers.
 def homepage(account_manager):
     while True:
-        account_manager.write_accounts()
+        account_manager.write_accounts()  #
         print("Create new account: 1")
         print("Login account: 2")
         try:
@@ -16,52 +18,55 @@ def homepage(account_manager):
             elif home == 2:
                 login(account_manager)
             elif home == 3:
-                return
+                return  # exit loop and program
             else:
                 print("Invalid number\n")
         except ValueError:
             print("Type numbers only\n")
 
 
+# handle user login process. user should input account number and pin
 def login(account_manager):
     while True:
         print("Input Login Info")
         account_number = str(input("Enter your account number: "))
-        account = account_manager.check_account(account_number)
+        account = account_manager.check_account(account_number)  # check if account exists in account list
         pin = int(input("Enter your pin: "))
+        # if account doesn't exist or pin doesn't match request the user to input valid data
         if account is None or not account.pin == pin:
             print("input valid account number or pin\n")
             break
         else:
             print("Login successful\n")
-        choose_action(account_manager, account)
+        choose_action(account_manager, account)  # navigate to user profile to choose actions
 
 
+# create new account functionality
 def create_new_account(account_manager):
     while True:
         print("Create new account")
-
+        # generate unique account number for new user
         while True:
             account_number = str(random.randint(10000, 99999))
             if not account_manager.check_account(account_number):
                 break  # Exit the loop if the account number is unique
             return account_number
-
+        # request user to input name and pin
         name = str(input("Enter your name: "))
         pin = int(input("Enter your pin: "))
         initial_balance = 0
         print()
-
+        # PIN code validation
         if not len(str(pin)) == 4:
             print("Invalid PIN. Please enter a 4-digit number.\n")
-            continue
         else:
-            account = Account(account_number, name, initial_balance, pin)
-            account_manager.add_account(account)
+            account = Account(account_number, name, initial_balance, pin)  # create Account class object
+            account_manager.add_account(account)  # add the newly created account to the list using the account manager
             print(f"Successfully created account: {account}\n")
         homepage(account_manager)
 
 
+# function to choose actions for logged-in users
 def choose_action(account_manager, account):
     while True:
         actions = """
@@ -73,9 +78,9 @@ Close account: 5
 Log out: 6
 Enter appropriate number: """
         try:
-            num = int(input(actions))
+            num = int(input(actions))  # action number input
             print()
-
+            # Conditional statement checks for the input number and executes corresponding actions
             if num == 1:
                 print(f"Balance: {account.getbalance()}$")
             elif num == 2:
@@ -89,8 +94,8 @@ Enter appropriate number: """
             elif num == 4:
                 while True:
                     new_pin = int(input("Enter the new pin (4 digits): "))
-                    if len(str(new_pin)) == 4 and str(new_pin).isdigit():
-                        if new_pin == account.get_pin():
+                    if len(str(new_pin)) == 4 and str(new_pin).isdigit():  # check if the input pin is a 4 digit
+                        if new_pin == account.get_pin():  # check if the new pin and old pin are the same
                             print(f"You already using this PIN")
                             break
                         account.change_pin(new_pin)
@@ -100,7 +105,7 @@ Enter appropriate number: """
             elif num == 5:
                 while True:
                     print("All your money will be withdrawn after entering PIN.")
-                    print("ENTER 1 to quit")
+                    print("To exit, please type '1'.")
                     pin = int(input("Enter your pin to close your account: "))
                     if pin == account.get_pin():
                         if account.getbalance() > 0:
@@ -114,20 +119,22 @@ Enter appropriate number: """
                         break
                     else:
                         print("Invalid PIN\n")
-
             elif num == 6:
                 homepage(account_manager)
             else:
                 print("Input number from 1 to 6\n")
+        # handle ValueError
         except ValueError:
             print("Invalid Value. Input numbers only")
 
 
+# main function to start the program
 def main():
-    account_manager = AccountManager()
-    account_manager.read_accounts()
-    homepage(account_manager)
+    account_manager = AccountManager()  # create AccountManager class object
+    account_manager.read_accounts()  # read json file
+    homepage(account_manager)  # run homepage function
 
 
+# checks whether the current script is being run as the main program, and if it is, it calls the main() function
 if __name__ == "__main__":
     main()
